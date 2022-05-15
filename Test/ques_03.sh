6b8a6d51cf8bf220fa7854fd87514197822405dd
rifatNR <Rifatnoor92@gmail.com>
2022-05-15 10:55:18 +0000

    changes

--- Test/ques_03.sh
+++ Test/ques_03.sh
@@ -6,6 +6,12 @@ then
 	exit 99;
 fi
 
+department=""
+belongs_to=""
+respectively_belongs_to=""
+sixth=5
+eighth=5
+
 for (( i = 0; i < ${#number}; ++i )); do
     digit=${number:$i:1}
     # echo $digit
@@ -15,14 +21,74 @@ for (( i = 0; i < ${#number}; ++i )); do
     	echo "5th: " $digit
     	case $digit in
 	  1 | 2 | 3)
-	    echo "SSSSSSSSS"
+	    #echo "CSE"
+	    department="CSE"
+	    ;;
+	  4 | 5 | 6)
+	    #echo "EEE"
+	    department="EEE"
+	    ;;
+  	  7 | 8 | 9)
+	    #echo "TEXTILE"
+	    department="TEXTILE"
 	    ;;
 	  *)
-	    echo "WWWWWW"
+	    #echo "---"
 	    ;;
 	esac
 
     fi
+    
+    if [ $i -eq 8 ]
+    then
+    	echo "9th: " $digit
+    	case $digit in
+	  4)
+	    #echo "LECTURER"
+	    belongs_to="LECTURER"
+	    ;;
+	  5)
+	    #echo "SENIOR LECTURER"
+    	    belongs_to="SENIOR LECTURER"
+	    ;;
+  	  6)
+	    #echo "ASSOCIATE PROFESSOR"
+    	    belongs_to="ASSOCIATE PROFESSOR"
+	    ;;
+    	  7)
+	    #echo "PROFESSOR"
+    	    belongs_to="PROFESSOR"
+	    ;;
+	  *)
+	    #echo "---"
+	    ;;
+	esac
+    fi
+    
+    if [ $i -eq 5 ]
+    then
+    	echo "6th: " $digit
+    	sixth=$digit
+    fi
+    
+    if [ $i -eq 5 ]
+    then
+	echo "8th: " $digit
+    	eighth=$digit
+    fi
 done
+
+if [[ $sixth -eq 0 ]] && [[ $eighth -eq 0 ]]
+then
+	respectively_belongs_to="PROGRAM COORDINATOR"
+fi
+
+
+if [[ $sixth -eq 9 ]] && [[ $eighth -eq 9 ]]
+then
+	respectively_belongs_to="CHAIR PERSON"
+fi
+
+echo "Dept. of ${department}, ${belongs_to} & ${respectively_belongs_to}"
 
 
